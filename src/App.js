31986d0a533baccdbb0a88ea606ebdee8b25f75a
georgeboholteanu <george.boholteanu@gmail.com>
2023-03-03 17:30:10 +0000

    added conditional rendering between pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import './App.css';
+import { Home } from './components/Home';
 import { Header } from './components/Header';
 import { Banner } from './components/Banner';
 import { About } from './components/About';
 import { Work } from './components/Work';
 import { Contact } from './components/Contact';
 import { Nav } from './components/Nav';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="bg-site bg-no-repeat bg-cover overflow-hidden">  
-      <Header/>   
-      <Banner/>   
-      <Nav/>   
-      <About/>   
-      <Work/>   
-      <Contact/>   
-      <div className='h-[4000px]'></div>
-    </div>
+    // <div className="bg-site">  
+    //   <Header/>   
+    //   <Banner/>   
+    //   <Nav/>
+    //   <Home/>   
+    //   <About/>   
+    //   <Work/>   
+    //   <Contact/>   
+    //   <div className='h-[4000px]'></div>
+    // </div>
+    <Router>
+      <div>
+        <Nav />
+        {/* Wrap Route elements in a Routes component */}
+        <Routes>
+          {/* Define routes using the Route component to render different page components at different paths */}
+          {/* Define a default route that will render the Home component */}
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="work" element={<Work />} />
+          {/* Define a route that will have descendant routes */}
+          <Route path="contact/*" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,26 +1,35 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 // import react icons
 import { FaEnvelope, FaDraftingCompass, FaChalkboardTeacher, FaHome } from "react-icons/fa";
 
-export const Nav = () => {
+
+export const Nav = (props) => {
   return (
     <nav className='fixed bottom-2 lg-bottom-8 w-full overflow-hidden z-50'>
       <div className="container mx-auto ">
         {/*nav inner*/}
         <div className='w-full  bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] flex px-5 justify-between items-center text-2xl text-white/50'>
           <ul className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
-            <li><a href="/" role='button'><FaHome/></a></li>
+            <li>
+              <NavLink to="/" end className={({isActive}) => isActive ? 'nav-link active' : 'nav-link'}><FaHome/></NavLink>
+            </li>
           </ul>
           <ul className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
-            <li><a href="#about" role='button'><FaChalkboardTeacher/></a></li>
+            <li>
+              <NavLink to="about" end className={({isActive}) => isActive ? 'nav-link active' : 'nav-link'}><FaChalkboardTeacher/></NavLink>
+            </li>
           </ul>
           <ul className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
-            <li><a href="#work" role='button'><FaDraftingCompass/></a></li>
+            <li>
+              <NavLink to="work" end className={({isActive}) => isActive ? 'nav-link active' : 'nav-link'}><FaDraftingCompass/></NavLink>
+            </li>
           </ul>
           <ul className='cursor-pointer w-[60px] h-[60px] flex items-center justify-center'>
-            <li><a href="#contact" role='button'><FaEnvelope/></a></li>
-          </ul>
-        
+            <li>
+              <NavLink to="contact" end className={({isActive}) => isActive ? 'nav-link active' : 'nav-link'}><FaEnvelope/></NavLink>
+            </li>
+          </ul>        
         </div>
       </div>
     </nav>

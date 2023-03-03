8bbfed785ba61d8ef54546dcccb915a848756ed6
georgeboholteanu <george.boholteanu@gmail.com>
2023-03-03 13:50:11 +0000

    cofigured react and tailwind

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import { Navbar } from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">  
+      <h1 className="text-2xl font-bold underline">
+        Hello world!
+      </h1>
+
+      <Navbar/>   
+    </div>
+  );
+}
+
+export default App;

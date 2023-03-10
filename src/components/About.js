f9bc535c01da501337e7b9d20f7054b4d7027901
georgeboholteanu <george.boholteanu@gmail.com>
2023-03-10 22:07:08 +0000

    style improved on the about and gallery components

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,66 @@
-import React from 'react'
+import React from 'react';
+
+const skills = [
+  { name: 'HTML5', width: '80%' },
+  { name: 'CSS3', width: '80%' },
+  { name: 'JavaScript', width: '55%' },
+  { name: 'Bootstrap', width: '70%' },
+  { name: 'Tailwind', width: '70%' },
+  { name: 'jQuery', width: '50%' },
+  { name: 'Node JS', width: '35%' },
+  { name: 'React', width: '45%' },
+  { name: 'GitHub', width: '65%' },
+];
+
+const SkillBar = ({ name, width }) => (
+  <div className="text-gray-300 m-5">
+    <p>{name}</p>
+    <div className="w-60 bg-gray-300 rounded-full h-2.5 dark:bg-gray-700">
+      <div
+        className="bg-cyan-500 h-2.5 rounded-full"
+        style={{ width }}
+        aria-label={`${name} proficiency`}
+      ></div>
+    </div>
+  </div>
+);
 
 export const About = () => {
   return (
-    <section id='about'>
-      <div className=" dark:bg-gray-900 justify-center max-w-screen-lg">
-      <h2 className='m-10 text-4xl tracking-tight font-extrabold text-center text-white dark:text-dark lg:text-4xl md:text-3xl sm:text-2xl'>About me</h2>
+    <section id="about">
+      <div className="dark:bg-gray-900 ">
+        <header>
+          <h2 className="lg:mt-10 mt-10 tracking-tight font-extrabold text-center text-white dark:text-dark lg:text-4xl md:text-3xl text-3xl">
+            About me
+          </h2>
+        </header>
 
-        <img src='/assets/images/exterior_hero.jpg' alt='contact-me' className='block rounded-3xl mb-4'/> 
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-          mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-          porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-          semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-          rhoncus mollis diam, sit amet facilisis lectus blandit at.
-        </p>
+        <main className="lg:mt-30 lg:mb-10 md:mb-60 mb-4">
+          <div className="flex flex-col lg:flex-row md:flex-row items-center justify-center">
+            <img
+              src="/assets/images/exterior_hero.jpg"
+              alt="Contact me CGI"
+              className="lg:rounded-3xl md:rounded-3xl justify-center lg:w-[550px] md:w-[550px] w-full lg:mt-20 mt-10"
+            />
+            <p className="text-stone-300 italic m-5 lg:m-10 lg:pt-10 md:pt-5 pt-4 lg:w-[280px] w-[280px] text-justify">
+            I am an aspiring junior software developer, fresh out of an intensive 18 weeks coding bootcamp and looking to build on my previous work experience in architectural visualization.
+            </p>
+          </div>
+        </main>
+
+        {/* SKILLS */}
+        <article className="justify-center mb-10">
+          <h3 className="text-amber-400 text-2xl font-extrabold pt-10 justify-center text-center">
+            Technical skills
+          </h3>
+          <div className="grid lg:grid-cols-3 lg:gap-2 gap-0 justify-center">
+            {skills.map((skill) => (
+              <SkillBar key={skill.name} {...skill} />
+            ))}
+          </div>
+        </article>
       </div>
     </section>
-  )
-}
+  );
+};
+
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,27 +6,26 @@ export const Gallery = () => {
   return (
     <section id="gallery">
       <h2 className='m-10 text-4xl tracking-tight font-extrabold text-center text-white dark:text-dark lg:text-4xl md:text-3xl sm:text-2xl'>Gallery</h2>
-      <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
+      <div className="max-w-screen-xl mx-auto grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 px-4">
           {data.map((project) => (
-
-            <div className="max-w-sm h-[340px] m-1 bg-gray-300  border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" key={project.id}>
+            <div className="max-w-sm bg-gray-300 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" key={project.id}>
               <a href={project.url}>
-                <img className="gallery-img rounded-t-lg h-[280px] w-full object-cover object-top border border-grey bg-center" src={project.thumb} alt="" />
+                <img className="gallery-img w-full h-56 object-cover object-top border border-grey bg-center rounded-t-lg" src={project.thumb} alt="" />
               </a>
-              <div className="flex p-4 justify-between">
-                <a href="project.url">
-                <h6 className="mb-1 text-md  tracking-tight text-gray-900 dark:text-white">{project.name}</h6>
+              <div className="p-4">
+                <a href={project.url}>
+                  <h6 className="mb-1 text-md  tracking-tight text-gray-900 dark:text-white">{project.name}</h6>
                 </a>
-
                 <a href={project.url} className="inline-flex items-center px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Read more
-                    <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"></path></svg>
+                  Read more
+                  <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"></path>
+                  </svg>
                 </a>
               </div>
             </div>
-          ))} 
+          ))}
       </div>
     </section>
   );
 };
-
